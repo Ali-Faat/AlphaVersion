d97b9238bc2e6f7918cec2e660d0d9b487c3e975
Alliiro <alison.tierris@ark-isp.com.br>
2024-07-04 20:08:45 -0300

    upgrade
    upgrade da nova porta do banco de dados.

--- backend/database.py
+++ backend/database.py
@@ -4,6 +4,7 @@ def get_db_connection():
     # Substitua pelos seus dados de conexão
     mydb = mysql.connector.connect(
         host="localhost",
+        port="5004",
         user="Ali",
         password="@Alison2004",
         database="goalcast"  # Nome do seu banco de dados
