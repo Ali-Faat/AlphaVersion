67e621ee9920fc3ddb6e8ff55ac7ae82d9be3ad2
Alliiro <alison.tierris@ark-isp.com.br>
2024-07-13 18:19:07 -0300

    Upgrade
    Upgrade da api/login

--- frontend/pages/login/login.js
+++ frontend/pages/login/login.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const password = document.getElementById('password').value;
 
         try {
-            const response = await fetch('http://138.99.160.212:5000//api/login', {
+            const response = await fetch('http://138.99.160.212:5000/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
