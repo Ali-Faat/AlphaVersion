afc1ecedd2ef2c23a40fafd3965f090df17b7f1e
Alliiro <alison.tierris@ark-isp.com.br>
2024-08-02 09:01:13 -0300

    correções no encaminhamento de emails

--- backend/email_verification.py
+++ backend/email_verification.py
@@ -6,15 +6,13 @@ from flask import current_app
 def send_verification_email(email, verification_link, nome_completo):
     from_email = current_app.config['MAIL_USERNAME']
     password = current_app.config['MAIL_PASSWORD']
-
-    # Garantir que o assunto do e-mail está codificado em UTF-8
-    subject = 'Confirmação de Cadastro - GoalCast'
-    subject = str(subject).encode('utf-8')
+    mail_server = current_app.config['MAIL_SERVER']
+    mail_port = current_app.config['MAIL_PORT']  # Pode ser 25 para conexões não criptografadas
 
     msg = MIMEMultipart()
     msg['From'] = from_email
     msg['To'] = email
-    msg['Subject'] = subject.decode('utf-8')  # Decode de volta para string após codificação
+    msg['Subject'] = 'Confirmação de Cadastro - GoalCast'
 
     body = f"""
     Olá {nome_completo},
@@ -23,19 +21,16 @@ def send_verification_email(email, verification_link, nome_completo):
 
     Por favor, clique no link abaixo para verificar seu e-mail:
 
-    {verification_link}
+    <a href="{verification_link}">Clique aqui para verificar seu e-mail</a>
     """
 
-    # Certificar que o corpo do e-mail está codificado em UTF-8
-    msg.attach(MIMEText(body, 'plain', 'utf-8'))
+    msg.attach(MIMEText(body, 'html', 'utf-8'))
 
     try:
-        server = smtplib.SMTP(current_app.config['MAIL_SERVER'], current_app.config['MAIL_PORT'])
-        server.starttls()
-        server.login(from_email, password)
-        text = msg.as_string()
-        server.sendmail(from_email, email, text)
-        server.quit()
+        # Conectar ao servidor SMTP sem usar TLS
+        with smtplib.SMTP(mail_server, mail_port) as server:
+            server.login(from_email, password)
+            server.sendmail(from_email, email, msg.as_string())
         print(f'E-mail de verificação enviado para {email}')
     except Exception as e:
         print(f'Erro ao enviar e-mail: {e}')
