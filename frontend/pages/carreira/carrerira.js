331cf8ee6566c52d5ce99ce4b71e1046b0897383
Alliiro <alison.tierris@ark-isp.com.br>
2024-09-11 11:00:04 -0300

    carreira

---
+++ frontend/pages/carreira/carrerira.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-arenas').addEventListener('click', function() {
+    window.location.href = '../quadras/quadras.html'; // Substitua pelo link de destino
+});
+
+document.getElementById('btn-perfil').addEventListener('click', function() {
+    window.alert('Em breve essa funcionalidade estará disponível!')
+});
+
+document.getElementById('btn-campeonato').addEventListener('click', function() {
+    window.alert('Em breve essa funcionalidade estará disponível!')
+});
+
+document.getElementById('btn-ajustes').addEventListener('click', function() {
+    window.alert('Em breve essa funcionalidade estará disponível!')
+});
+
+document.getElementById('btn-live').addEventListener('click', function() {
+    window.alert('Em breve essa funcionalidade estará disponível!')
+});
