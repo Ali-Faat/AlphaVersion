14f9646b16c286d0440c622560332614bd401dc4
Alliiro <alison.tierris@ark-isp.com.br>
2024-09-11 14:36:29 -0300

    requisição banco 1
    requisição em teste local de como adicionar as informações do perfil de jogador no banco de dados.

---
+++ frontend/pages/PerfilJogador/perfil.js
@@ -0,0 +1,43 @@
+function enviarDados() {
+    // Captura os dados dos inputs
+    const nickname = document.getElementById('nickname').value;
+    const nome = document.getElementById('nome').value;
+    const nascimento = document.getElementById('nascimento').value;
+    const descricao = document.getElementById('descricao').value;
+    const numero = document.getElementById('numero').value;
+    const posicao = document.getElementById('posicao').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const pePref = document.getElementById('pePref').value;
+
+    // Dados para enviar na requisição
+    const dados = {
+        user: nickname,
+        nascimento: nascimento,
+        bio: descricao,
+        nCamiseta: parseInt(numero),
+        posicao: posicao,
+        altura: parseFloat(altura),
+        peso: parseFloat(peso),
+        peDominante: pePref
+    };
+
+    // Faz a requisição POST para a API Flask
+    fetch('http://192.168.1.12:5000/add_jogador', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dados), // Converte os dados para JSON
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Exibe uma mensagem de sucesso ou erro
+        alert('Jogador adicionado com sucesso!');
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert('Ocorreu um erro ao adicionar o jogador.');
+    });
+}
