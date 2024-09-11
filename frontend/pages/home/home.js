1feb253fb79073f1300969621c83219f10e716a5
nattanVil <natan.vilela@ark-isp.com.br>
2024-09-11 15:40:47 -0300

    last

--- frontend/pages/home/home.js
+++ frontend/pages/home/home.js
@@ -2,8 +2,8 @@ document.getElementById('btn-arenas').addEventListener('click', function() {
     window.location.href = '../quadras/quadras.html'; // Substitua pelo link de destino
 });
 
-document.getElementById('btn-perfil').addEventListener('click', function() {
-    window.alert('Em breve essa funcionalidade estará disponível!')
+document.getElementById('btn-carreira').addEventListener('click', function() {
+    window.location.href = '../Perfiljogador/perfil.html'; // Substitua pelo link de destino
 });
 
 document.getElementById('btn-campeonato').addEventListener('click', function() {
